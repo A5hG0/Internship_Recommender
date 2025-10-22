e9c2dc8ae64c06e27fc074d9642b89709a0068e2
A5hG0 <ashish522005@gmail.com>
2025-10-22 23:40:55 +0530

    Updated code

--- notebooks/Data_scraping.py
+++ notebooks/Data_scraping.py
@@ -3,37 +3,77 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-base_url = "https://internshala.com/internships/machine-learning-internship"
+# base_url = "https://internshala.com/internships/machine-learning-internship"
 
-titles, companies, locations, skills = [], [], [], []
+# titles, companies, locations, skills = [], [], [], []
 
-# fetch first few pages
-for page in range(1, 4):
-    url = f"{base_url}/page-{page}"
-    print("Scraping:", url)
-    res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-    soup = BeautifulSoup(res.text, 'html.parser')
+# # fetch first few pages
+# for page in range(1, 4):
+#     url = f"{base_url}/page-{page}"
+#     print("Scraping:", url)
+#     res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+#     soup = BeautifulSoup(res.text, 'html.parser')
 
-    # find all internship cards
-    internships = soup.find_all('div', class_='individual_internship')
+#     # find all internship cards
+#     internships = soup.find_all('div', class_='individual_internship')
 
-    for intern in internships:
-        # extract data
-        title = intern.find('h3', class_='job-internship-name')
-        company = intern.find('p', class_='company-name')
-        location = intern.find('div', class_='row-1-item locations')
-        skill_div = intern.find('div', class_='job_skills')
+#     for intern in internships:
+#         # extract data
+#         title = intern.find('h3', class_='job-internship-name')
+#         company = intern.find('p', class_='company-name')
+#         location = intern.find('div', class_='row-1-item locations')
+#         skill_div = intern.find('div', class_='job_skills')
 
-        titles.append(title.text.strip() if title else "")
-        companies.append(company.text.strip() if company else "")
-        locations.append(location.text.strip() if location else "")
-        skills.append(skill_div.text.strip() if skill_div else "")
+#         titles.append(title.text.strip() if title else "")
+#         companies.append(company.text.strip() if company else "")
+#         locations.append(location.text.strip() if location else "")
+#         skills.append(skill_div.text.strip() if skill_div else "")
 
-    time.sleep(1)  # be polite — delay between requests
+#     time.sleep(1)  # be polite — delay between requests
     
-base_url = "https://internshala.com/internships/data-science-internship"
+# base_url = "https://internshala.com/internships/data-science-internship"
 
-for page in range(1, 9):
+# for page in range(1, 9):
+#     url = f"{base_url}/page-{page}"
+#     print("Scraping:", url)
+#     res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+#     soup = BeautifulSoup(res.text, 'html.parser')
+
+#     # find all internship cards
+#     internships = soup.find_all('div', class_='individual_internship')
+
+#     for intern in internships:
+#         # extract data
+#         title = intern.find('h3', class_='job-internship-name')
+#         company = intern.find('p', class_='company-name')
+#         location = intern.find('div', class_='row-1-item locations')
+#         skill_div = intern.find('div', class_='job_skills')
+
+#         titles.append(title.text.strip() if title else "")
+#         companies.append(company.text.strip() if company else "")
+#         locations.append(location.text.strip() if location else "")
+#         skills.append(skill_div.text.strip() if skill_div else " ")
+#         # print(skills)
+
+#     time.sleep(1)  # be polite — delay between requests
+# # print(title)
+# # save to DataFrame
+# df = pd.DataFrame({
+#     'Title': titles,
+#     'Company': companies,
+#     'Location': locations,
+#     'Skills': skills
+# })
+
+# df.to_csv("internships.csv", index=False)
+# print("✅ Scraping complete! Data saved to internships.csv")
+
+
+existing_df = pd.read_csv("Z:\Python\Internship_Recommender\data\internships_cleaned.csv")
+
+base_url = "https://internshala.com/internships/artificial-intelligence-ai-internship"
+
+for page in range(1, 5):
     url = f"{base_url}/page-{page}"
     print("Scraping:", url)
     res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
---
+++ notebooks/data_cleaning.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+o_df = pd.read_csv("Z:/Python/Internship_Recommender/data/internships.csv")
+df = o_df.copy() #Working on copy of the dataset
+nul = df.isna().sum().sort_values(ascending = False)
+dup = df.duplicated().sum()
+# print(nul)
+# print(dup)
+print(df.isnull().sum())
+# print(df.shape)
+df = df.dropna()
+# print(df.shape)
+print(df.isnull().sum())
+df.to_csv("Z:/Python/Internship_Recommender/data/internships_cleaned.csv")
+# nul = df.isna().sum().sort_values(ascending = False)
+# dup = df.duplicated().sum()
+# print(nul)
+# print(dup)
+# print(df.head())
